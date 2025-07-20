605816f60e06d6ccf0b6a5d4af7a87e9d9db3b73
Reiner Knudsen <reinerknudsen@me.com>
2025-07-20 18:25:02 +0200

    header designed

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,14 @@ import './App.css';
 
 function App() {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div
+      className='min-h-screen flex flex-col bg-slate-100 rounded-xl'
+      style={{
+        paddingTop: 'env(safe-area-inset-top)',
+        paddingBottom: 'env(safe-area-inset-bottom)',
+        paddingLeft: 'env(safe-area-inset-left)',
+        paddingRight: 'env(safe-area-inset-right)',
+      }}>
       <Header />
       <main className='flex-1 py-8'>
         <Outlet />
--- src/lib/components/Header.jsx
+++ src/lib/components/Header.jsx
@@ -1,7 +1,11 @@
 export function Header() {
   return (
-    <div className='fixed top-0 left-0 right-0 flex flex-row justify-between items-center px-8 h-8 py-4'>
-      Header section
+    <div
+      className='sticky top-0 left-0 right-0 py-4'
+      style={{ paddingTop: 'env(safe-area-inset-top)' }}>
+      <div class='h-12 flex flex-row justify-center items-center bg-accent w-full'>
+        <h1 class='text-lg tracking-wider text-accent-foreground font-semibold '>My Carsharing</h1>
+      </div>
     </div>
   );
 }
